2e288301ef1b242984b6e10a4cdf3a41f709b93e
linu4u <linu4u@naver.com>
2015-07-29 13:33:12 +0900

    150729-133311 -

--- m/js/common.js
+++ m/js/common.js
@@ -28,12 +28,11 @@ $(document).ready(function() {
   $(".accordion").click(function() {
     if( $(this).hasClass("on")) {
       $(this).removeClass("on");
-
     } else {
       $(this).addClass("on");
     };
-
   });
+
 
 
 });
